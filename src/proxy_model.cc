6190c23591b9a3551348c5fd8d0130badc6f0f1f
Ginakira <ginakira@foxmail.com>
2021-06-25 08:49:51 +0800

    重构代理模型部分代码

--- src/proxy_model.cc
+++ src/proxy_model.cc
@@ -12,64 +12,105 @@ ProxyModel::ProxyModel(QObject *parent) : QIdentityProxyModel(parent) {}
 
 QVariant ProxyModel::data(const QModelIndex &index, int role) const {
 //    const QModelIndex source_index = mapToSource(index);
-    if ((index.column() == NAV_TIME_COL ||
-         index.column() == VALUATION_TIME_COL) && role == Qt::DisplayRole) {
-        // 净值时间、估值时间不显示年份
-        QString text = sourceModel()->data(index).toString();
-        text = text.mid(5); // 截取掉年份
-        return text;
-    } else if ((index.column() == HOLDING_EARNINGS_COL ||
-                index.column() == HOLDING_EARNING_RATE_COL ||
-                index.column() == NAV_GAINS_COL ||
-                index.column() == VALUATION_GAINS_COL ||
-                index.column() == EXPECTED_EARNINGS_COL) &&
-               role == Qt::TextColorRole) {
-        // 持仓收益、收益率、净值涨跌、估值涨跌、预计收益颜色
-        double rate = sourceModel()->data(index).toDouble();
-        if (rate < 0) {
-            return QVariant::fromValue(QColor(Qt::darkGreen));
-        } else {
-            return QVariant::fromValue(QColor(Qt::red));
+    int col = index.column();
+    if (col == NAV_TIME_COL) { // 净值时间列
+        if (role == Qt::DisplayRole) { // 净值时间不显示年份
+            QString text = sourceModel()->data(index).toString();
+            text = text.mid(5); // 截取掉年份
+            return text;
+        } else if (role == Qt::TextColorRole) { // 净值时间颜色
+            QModelIndex nav_gains_index = sourceModel()->index(index.row(), NAV_GAINS_COL);
+            double rate = sourceModel()->data(nav_gains_index).toDouble();
+            if (rate < 0) {
+                return QVariant::fromValue(QColor(Qt::darkGreen));
+            } else {
+                return QVariant::fromValue(QColor(Qt::red));
+            }
         }
-    } else if ((index.column() == HOLDING_EARNING_RATE_COL ||
-                index.column() == NAV_GAINS_COL ||
-                index.column() == VALUATION_GAINS_COL) &&
-               role == Qt::DisplayRole) {
-        // 收益率、净值涨跌、估值涨跌百分号和保留两位
-        double rate = sourceModel()->data(index).toDouble();
-        return QString("%1%2%").arg(rate >= 0 ? "+" : "",
-                                    QString::number(rate, 'f', 2));
-    } else if ((index.column() == NAV_COL ||
-                index.column() == NAV_TIME_COL)
-               && role == Qt::TextColorRole) {
-        // 单位净值、净值时间颜色
-        QModelIndex nav_gains_index = sourceModel()->index(index.row(), NAV_GAINS_COL);
-        double rate = sourceModel()->data(nav_gains_index).toDouble();
-        if (rate < 0) {
-            return QVariant::fromValue(QColor(Qt::darkGreen));
-        } else {
-            return QVariant::fromValue(QColor(Qt::red));
+    } else if (col == HOLDING_EARNING_RATE_COL ||
+               col == NAV_GAINS_COL ||
+               col == VALUATION_GAINS_COL) { // 持有收益率、净值涨跌、估值涨跌列
+        if (role == Qt::DisplayRole) { // 正负号、百分号、保留两位
+            double rate = sourceModel()->data(index).toDouble();
+            return QString("%1%2%").arg(rate >= 0 ? "+" : "",
+                                        QString::number(rate, 'f', 2));
+        } else if (role == Qt::TextColorRole) { // 颜色
+            double rate = sourceModel()->data(index).toDouble();
+            if (rate < 0) {
+                return QVariant::fromValue(QColor(Qt::darkGreen));
+            } else {
+                return QVariant::fromValue(QColor(Qt::red));
+            }
         }
-    } else if (index.column() == VALUATION_COL && role == Qt::TextColorRole) {
-        // 盘中估值颜色
-        QModelIndex valuation_gains_index = sourceModel()->index(index.row(), VALUATION_GAINS_COL);
-        double rate = sourceModel()->data(valuation_gains_index).toDouble();
-        if (rate < 0) {
-            return QVariant::fromValue(QColor(Qt::darkGreen));
-        } else {
-            return QVariant::fromValue(QColor(Qt::red));
+    } else if (col == HOLDING_EARNINGS_COL
+               || col == EXPECTED_EARNINGS_COL) { // 持仓收益列、预期收益列
+        if (role == Qt::DisplayRole) { // 保留两位
+            double rate = sourceModel()->data(index).toDouble();
+            return QString("%1%2").
+                    arg(rate
+                        >= 0 ? "+" : "",
+                        QString::number(rate,
+                                        'f', 2));
+        } else if (role == Qt::TextColorRole) { //颜色
+            double rate = sourceModel()->data(index).toDouble();
+            if (rate < 0) {
+                return
+                        QVariant::fromValue(QColor(Qt::darkGreen)
+                        );
+            } else {
+                return
+                        QVariant::fromValue(QColor(Qt::red)
+                        );
+            }
         }
-    } else if ((index.column() == HOLDING_EARNINGS_COL ||
-                index.column() == EXPECTED_EARNINGS_COL)
-               && role == Qt::DisplayRole) {
-        // 持有收益、预计收益保留两位
-        double rate = sourceModel()->data(index).toDouble();
-        return QString("%1%2").arg(rate >= 0 ? "+" : "",
-                                   QString::number(rate, 'f', 2));
-    } else if ((index.column() == HOLDING_TOTAL_COST_COL) && role == Qt::DisplayRole) {
-        // 持有金额保留整数
-        return QString::number(sourceModel()->data(index).toDouble(), 'f', 0);
-    } else {
-        return QIdentityProxyModel::data(index, role);
+    } else if (col == NAV_COL) { // 净值列
+        if (role == Qt::TextColorRole) { // 颜色
+            QModelIndex nav_gains_index = sourceModel()->index(index.row(), NAV_GAINS_COL);
+            double rate = sourceModel()->data(nav_gains_index).toDouble();
+            if (rate < 0) {
+                return
+                        QVariant::fromValue(QColor(Qt::darkGreen)
+                        );
+            } else {
+                return
+                        QVariant::fromValue(QColor(Qt::red)
+                        );
+            }
+        }
+    } else if (col == VALUATION_COL) { // 估值列
+        if (role == Qt::TextColorRole) { // 颜色
+            QModelIndex valuation_gains_index = sourceModel()->index(index.row(), VALUATION_GAINS_COL);
+            double rate = sourceModel()->data(valuation_gains_index).toDouble();
+            if (rate < 0) {
+                return
+                        QVariant::fromValue(QColor(Qt::darkGreen)
+                        );
+            } else {
+                return
+                        QVariant::fromValue(QColor(Qt::red)
+                        );
+            }
+        }
+    } else if (col == HOLDING_TOTAL_COST_COL) { // 持有金额列
+        if (role == Qt::DisplayRole) { // 保留整数
+            return QString::number(sourceModel()->data(index).toDouble(), 'f', 0);
+        }
+    } else if (col == REMARKS_COL) {
+        if (role == Qt::ToolTipRole) {
+            return sourceModel()->data(index).toString();
+        }
     }
+    return QIdentityProxyModel::data(index, role);
 }
+
+//QVariant ProxyModel::data(const QModelIndex &index, int role) const {
+
+
+//
+//    } else if ((index.column() == HOLDING_TOTAL_COST_COL) && role == Qt::DisplayRole) {
+//        // 持有金额保留整数
+//        return QString::number(sourceModel()->data(index).toDouble(), 'f', 0);
+//    } else {
+//        return QIdentityProxyModel::data(index, role);
+//    }
+//}
